1bdb496bd78ca1666b06b47420ddf9ee8b530f4a
Jason Kelly <jkelly@cambridge-intl.com>
2012-11-05 15:26:06 -0500

    Initial code commit

---
+++ TestApplication/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TestApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                //Disconnect();
+            }
+            
+
+        }
+    }
+}
